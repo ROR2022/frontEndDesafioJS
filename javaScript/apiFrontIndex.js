b2abd413186ee1f68d35d08d1723d9aadf0fe637
ROR2022 <rami.ror279@gmail.com>
2023-03-02 21:09:33 -0600

    cambios localStorage

--- javaScript/apiFrontIndex.js
+++ javaScript/apiFrontIndex.js
@@ -36,10 +36,23 @@ getLastPost()
     console.log(err);
   });
 
+  const botonCreatePost = document.querySelector('#botonCreatePost');
+
   if(dataUserLogged.id){
     console.log('dataUserLogged:..',dataUserLogged);
     const avatarUserLogo = document.querySelector('#avatarUserLogo');
     if (avatarUserLogo){
       avatarUserLogo.src= dataUserLogged.avatarUrl;
     }
+    
   }
+  
+  if (botonCreatePost&&dataUserLogged.id){
+    botonCreatePost.className = 'btn btn-outline-primary';
+}else if(botonCreatePost&&!dataUserLogged.id){
+      botonCreatePost.className = 'd-none';
+  }
+
+  
+
+  
